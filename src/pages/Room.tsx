576e177bd3404acf0f08814c1685e3601e4aea98
misha <mistetsmit05@gmail.com>
2025-07-30 10:04:17 +0300

    add time_id

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -5,11 +5,13 @@ import {useEffect, useState} from "react";
 import {getToken} from "../api/video/api.ts";
 import RoomConference from "../components/live-kit-custom/RoomConference.tsx";
 import {RoomProvider} from "../components/RoomContext.tsx";
+import Cookies from 'universal-cookie';
 
 const RoomPage = () => {
 
 	const params = useParams();
 	const roomId = params?.roomId || "";
+	const cookies = new Cookies();
 
 	const [loading, setLoading] = useState(true);
 	const [token, setToken] = useState<string | null>(null);
@@ -17,8 +19,12 @@ const RoomPage = () => {
 	useEffect(() => {
 		const fetchToken = async () => {
 			try {
-
-				const response = await getToken({room: roomId, identity: 'user-' + Date.now(),});
+				let time_id = cookies.get("time_id");
+				if (!time_id) {
+					time_id = Date.now();
+					cookies.set("time_id", time_id);
+				}
+				const response = await getToken({room: roomId, identity: 'user-' + time_id,});
 
 				setToken(response.token);
 				setLoading(false);
